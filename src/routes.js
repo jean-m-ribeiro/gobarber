6f5e11176b19421294cdf8fd524c46250ebdc374
Jean Ribeiro <jeanmendesrib@gmail.com>
2019-08-18 16:00:39 -0300

    Feature: add model loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Jean' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Jean Ribeiro',
+    email: 'jeanmendesrib@gmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
